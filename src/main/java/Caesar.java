e1b1a181e67967b07e00a97bb9babff265cf2179
Sentientnapkin <24svargas@athenian.org>
2021-09-17 17:05:05 -0700

    only caesar encryption

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
-public class MyMain {
+public class Caesar {
     // Given a String, encrypts the String with the Caesar cipher
     // and returns the encrypted String
     // Ex. encryptCaesar("Hello, World!") => "Khoor, Zruog!"
     public static String encryptCaesar(String message) {
         String encrypted = "";
-        for(int i = 0;i<message.length();i++){
+        for (int i = 0; i < message.length(); i++) {
             char ch = message.charAt(i);
-            if (ch>='A'&&ch<='W'||ch>='a'&&ch<='w')
-                encrypted += (char)(ch+3);
-            else if (ch>='X'&&ch<='Z'||ch>='x'&&ch<='z')
-                encrypted += (char)(ch-23);
+            if (ch >= 'A' && ch <= 'W' || ch >= 'a' && ch <= 'w')
+                encrypted += (char) (ch + 3);
+            else if (ch >= 'X' && ch <= 'Z' || ch >= 'x' && ch <= 'z')
+                encrypted += (char) (ch - 23);
             else encrypted += ch;
         }
         message = encrypted;
@@ -23,12 +23,12 @@ public class MyMain {
     // Ex. decryptCaesar("Khoor, Zruog!") => "Hello, World!"
     public static String decryptCaesar(String message) {
         String decrypted = "";
-        for(int i = 0;i<message.length();i++){
+        for (int i = 0; i < message.length(); i++) {
             char ch = message.charAt(i);
-            if (ch>='D'&&ch<='Z'||ch>='d'&&ch<='z')
-                decrypted += (char)(ch-3);
-            else if (ch>='A'&&ch<='C'||ch>='a'&&ch<='c')
-                decrypted += (char)(ch+23);
+            if (ch >= 'D' && ch <= 'Z' || ch >= 'd' && ch <= 'z')
+                decrypted += (char) (ch - 3);
+            else if (ch >= 'A' && ch <= 'C' || ch >= 'a' && ch <= 'c')
+                decrypted += (char) (ch + 23);
             else decrypted += ch;
         }
         message = decrypted;
@@ -42,12 +42,12 @@ public class MyMain {
         String encrypted = "";
         if (key == 0) return message;
         if (key >= 26) key %= 26;
-        for(int i = 0;i<message.length();i++){
+        for (int i = 0; i < message.length(); i++) {
             char ch = message.charAt(i);
-            if (ch>='A'&&ch<=(char)('Z'-key)||ch>='a'&&ch<=(char)('z'-key))
-                encrypted += (char)(ch+key);
-            else if (ch>=(char)('Z'-key+1)&&ch<='Z'||ch>=(char)('z'-key+1)&&ch<='z')
-                encrypted += (char)(ch-(26-key));
+            if (ch >= 'A' && ch <= (char) ('Z' - key) || ch >= 'a' && ch <= (char) ('z' - key))
+                encrypted += (char) (ch + key);
+            else if (ch >= (char) ('Z' - key + 1) && ch <= 'Z' || ch >= (char) ('z' - key + 1) && ch <= 'z')
+                encrypted += (char) (ch - (26 - key));
             else encrypted += ch;
         }
         message = encrypted;
@@ -61,12 +61,12 @@ public class MyMain {
         String decrypted = "";
         if (key == 0) return message;
         if (key >= 26) key %= 26;
-        for(int i = 0;i<message.length();i++){
+        for (int i = 0; i < message.length(); i++) {
             char ch = message.charAt(i);
-            if (ch>=(char)('Z'-(26-key))&&ch<=('Z')||ch>=(char)('z'-(26-key))&&ch<='z')
-                decrypted += (char)(ch-key);
-            else if (ch>='A'&&ch<=(char)('A'+key)||ch>='a'&&ch<=(char)('a'+key))
-                decrypted += (char)(ch+(26-key));
+            if (ch >= (char) ('Z' - (26 - key)) && ch <= ('Z') || ch >= (char) ('z' - (26 - key)) && ch <= 'z')
+                decrypted += (char) (ch - key);
+            else if (ch >= 'A' && ch <= (char) ('A' + key) || ch >= 'a' && ch <= (char) ('a' + key))
+                decrypted += (char) (ch + (26 - key));
             else decrypted += ch;
         }
         message = decrypted;
@@ -90,9 +90,8 @@ public class MyMain {
             int key = scan.nextInt();
             System.out.println(key);
             System.out.println("Here is your encrypted message: ");
-            System.out.println(encryptCaesarKey(message,key));
-        }
-        else if (command.equals("decrypt")) {
+            System.out.println(encryptCaesarKey(message, key));
+        } else if (command.equals("decrypt")) {
             System.out.println("Please enter your message to be decrypted: ");
             String message = scan.nextLine();
             System.out.println(message);
@@ -100,11 +99,11 @@ public class MyMain {
             int key = scan.nextInt();
             System.out.println(key);
             System.out.println("Here is your decrypted message: ");
-            System.out.println(decryptCaesarKey(message,key));
-        }
-        else {
+            System.out.println(decryptCaesarKey(message, key));
+        } else {
             System.out.println("Unknown command; please type either \"encrypt\" or \"decrypt\"");
         }
 
         scan.close();
     }
+}
