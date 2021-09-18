f17586987a3ec4fc293866cd5b4902d64825ca7d
Sentientnapkin <24svargas@athenian.org>
2021-09-18 11:51:24 -0700

    caesar and affnie

--- src/main/java/Affine.java
+++ src/main/java/Affine.java
@@ -2,29 +2,65 @@ import java.util.Scanner;
 
 public class Affine {
     public static String encryptAffine(String message) {
-        // REPLACE THIS WITH YOUR CODE
-        return message;
+        String encrypt = "";
+        for(int i = 0;i<message.length();i++){
+            char ch = message.charAt(i);
+            if (ch>='A'&&ch<='Z')
+                encrypt+=(char)(((3(ch-65))%26)+65);
+                else if (ch>='a'&&ch<='z')
+                encrypt+=(char)(((3(ch-97))%26)+97);
+                else encrypt+=(char)(ch);
+        }
+        return encrypt;
     }
 
     public static String decryptAffine(String message) {
-        // REPLACE THIS WITH YOUR CODE
-        return message;
+        String decrypt = "";
+        for(int i = 0;i<message.length();i++){
+            char ch = message.charAt(i);
+            if (ch>='A'&&ch<='Z')
+                decrypt+=(char)(((9(ch-65))%26)+65);
+            else if (ch>='a'&&ch<='z')
+                decrypt+=(char)(((9(ch-97))%26)+97);
+            else decrypt+=(char)(ch);
+        }
+        return decrypt;
     }
 
     public static String encryptAffineKeys(String message, int key1, int key2) {
-        // REPLACE THIS WITH YOUR CODE
-        return message;
+        String encrypt = "";
+        for(int i = 0;i<message.length();i++){
+            char ch = message.charAt(i);
+            if (ch>='A'&&ch<='Z')
+                encrypt+=(char)((((key1(ch-65))+key2)%26)+65);
+            else if (ch>='a'&&ch<='z')
+                encrypt+=(char)((((key1(ch-97))+key2)%26)+97);
+            else encrypt+=(char)(ch);
+        }
+        return encrypt;
     }
 
     public static int modularInverse(int x) {
-        // REPLACE THIS WITH YOUR CODE
-        return -1;
+        int g = 0;
+        while ((xg)%26!=1){
+            g++;
+        }
+        System.out.println(g);
+        return g;
     }
 
 
     public static String decryptAffineKeys(String message, int key1, int key2) {
-        // REPLACE THIS WITH YOUR CODE
-        return message;
+        String decrypt = "";
+        for(int i = 0;i<message.length();i++){
+            char ch = message.charAt(i);
+            if (ch>='A'&&ch<='Z')
+                decrypt+=(char)(((((modularInverse(key1)+26)%26)(((ch-65)-key2)+26)%26)%26)+65);
+            else if (ch>='a'&&ch<='z')
+                decrypt+=(char)(((((modularInverse(key1)+26)%26)*(((ch-97)-key2)+26)%26)%26)+97);
+            else decrypt+=(char)(ch);
+        }
+        return decrypt;
     }
 
     // Some basic testing code
